2941123c307c233d500876ceb0e5919c81014b0a
Kamal Mustafa <kamal@lalokalabs.co>
2022-03-29 19:04:37 +0800

    add example views

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "djangomix",
+    "{{cookiecutter.project_name}}",
 ]
 
 if DEBUG:
--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from {{cookiecutter.project_name}} import views
+
 urlpatterns = [
+    path("", views.index),
     path('admin/', admin.site.urls),
 ]
 
